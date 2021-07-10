38d1b33dc1a9cb8091377f6a9e868b9ea696b01b
hville <hugov@runbox.com>
2021-07-09 22:28:45 -0400

    logo

--- montecode/2d.js
+++ montecode/2d.js
@@ -6,25 +6,9 @@ export default function(ctx, stat, confidence) {
 				Ky = ctx.canvas.height / rs[rs.length-1] * (vb-va)/5 //*(stat.Q(0.9)-stat.Q(0.1))
 
 	ctx.save()
-	ctx.setTransform(1, 0, 0, -1, -va*Kx, ctx.canvas.height)
-	plotf(ctx, stat, Kx, Ky)
+	stat.plotf(ctx)
 	ctx.globalCompositeOperation = 'destination-out'
 	ctx.fill()
 	ctx.restore()
 	ctx.stroke()
-	ctx.beginPath()
-}
-
-function plotf(ctx, stat, Kx, Ky) {
-	const rs = stat.rs,
-				vs = stat.vs
-
-	let x=vs[0] * Kx
-	ctx.beginPath()
-	ctx.moveTo( x, 0 )
-	for (let i=0, j=1, y=0; j<rs.length; i=j++) {
-		ctx.lineTo( x, y = (rs[j]-rs[i])/(vs[j]-vs[i]) * Ky ) //up
-		ctx.lineTo( x = vs[j] * Kx, y ) //right
-	}
-	ctx.lineTo( x, 0 )
 }
