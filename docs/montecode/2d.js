72a2724a1036bfc4df273ab57fb8f63502579794
hville <hugov@runbox.com>
2023-02-21 21:24:18 -0500

    grrr

--- docs/montecode/2d.js
+++ docs/montecode/2d.js
@@ -1,12 +1,6 @@
-export default function(ctx, stat, confidence) {
-	const rs = stat.rs,
-				va = stat.Q( .5*(1-confidence) ),
-				vb = stat.Q( .5*(1+confidence) ),
-				Kx = ctx.canvas.width/(vb-va),
-				Ky = ctx.canvas.height / rs[rs.length-1] * (vb-va)/5 //*(stat.Q(0.9)-stat.Q(0.1))
-
+export default function(ctx, stat, xMin, xMax) {
 	ctx.save()
-	stat.plotf(ctx)
+	stat.plotf(ctx, xMin, xMax)
 	ctx.globalCompositeOperation = 'destination-out'
 	ctx.fill()
 	ctx.restore()
